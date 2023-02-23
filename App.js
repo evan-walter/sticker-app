4568a17222dde868800fdb3d6437ad8dc89b36b8
Evan Walter <evan.walter.software@gmail.com>
2023-02-23 12:49:55 -0800

    Extract image component, and add button component

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import Button from './components/Button'
+import ImageViewer from './components/ImageViewer'
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#fff' }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Image source={PlaceholderImage} style={styles.image} />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='Use this photo' />
+      </View>
       <StatusBar style='auto' />
     </View>
   )
@@ -26,9 +31,8 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
